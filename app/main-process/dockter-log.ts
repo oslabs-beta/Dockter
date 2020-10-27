3ee3d870a673c0d223cdd92581428320cf263d4e
Benjamin Brower <benbrower@Benjamins-MBP.lan>
2020-10-27 18:53:58 -0400

    stream working

--- app/main-process/dockter-log.ts
+++ app/main-process/dockter-log.ts
@@ -1,4 +1,4 @@
-import { ipcMain } from 'electron';
+import { ipcMain, webContents } from 'electron';
 import { db } from './db.ts';
 
 const Docker = require('dockerode');
@@ -10,7 +10,12 @@ const socket = io('http://localhost:8080');
 
 // socket.emit('initializeLogger');
 
+
 ipcMain.on('ready', (event, arg) => {
+  const content = webContents.getAllWebContents()[0];
+  // console.log('content,', content);
+  content.send('testmessage', 'hello from ipc main');
+
   docker.listContainers((err, containers) => {
     containers.forEach((container) => {
       if (container.Names[0] !== '/dockter-log')
@@ -18,16 +23,20 @@ ipcMain.on('ready', (event, arg) => {
     });
   });
 
-  socket.on('newLog', (log) => {
-    console.log('log: ', log);
-    const { containerId, message, timestamp, logLevel, stream } = log;
+  socket.on('newLog', (logGuy) => {
+    console.log('log: ', logGuy);
+    const { containerId, log, time, log_level, stream } = logGuy;
     db.serialize(() => {
       db.run(
-        `INSERT INTO logs (containerId, message, timestamp, logLevel, stream)
-        VALUES (${containerId}, ${message}, ${timestamp}, ${logLevel}, ${stream})
-        )`
+        `INSERT INTO logs (container_id, message, timestamp, log_level, stream)
+        VALUES ($1, $2, $3, $4, $5)`, [containerId, log, time, log_level, stream], (err) => {
+          if (err) {
+            console.log(err.message);
+          }
+          console.log('A row has been inserted corrrectly');
+        }
       );
     });
-    ipcMain.on('shipLog', (event.returnValue = log));
+    content.send('shipLog', logGuy);
   });
 });
