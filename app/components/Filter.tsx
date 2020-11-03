30d243be84dd6a75d47bce2c320d4839724091c5
Sam Kim <samuyyy@gmail.com>
2020-11-02 23:08:14 -0500

    Extract filter options from filter menu as its own component

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import CurrentFilters from './CurrentFilters.tsx';
+import CurrentFilters from './CurrentFilters';
+import FilterOptions from './FilterOptions';
 
 const Filter = (props) => {
   //a piece of local state to render the correct options to filter by
@@ -62,22 +63,12 @@ const Filter = (props) => {
               )}
             </span>
           </div>
-
-          {/* <!--
-					Dropdown panel, show/hide based on dropdown state.
-
-					Entering: "transition ease-out duration-100"
-					From: "transform opacity-0 scale-95"
-					To: "transform opacity-100 scale-100"
-					Leaving: "transition ease-in duration-75"
-					From: "transform opacity-100 scale-100"
-					To: "transform opacity-0 scale-95"
-				--> */}
-
           {isOpen && (
-            <div className="origin-top-right absolute left-0 mt-2 py-1 w-56 rounded-md shadow-lg">
-              <div className="rounded-md bg-white shadow-xs">
-                <div
+            <FilterOptions setSelection={setSelection} setIsOpen={setIsOpen} />
+          )}
+          {/* <div className="origin-top-right absolute left-0 mt-2 py-1 w-56 rounded-md shadow-lg">
+              <div className="rounded-md bg-white shadow-xs"> */}
+          {/* <div
                   className="py-1"
                   role="menu"
                   aria-orientation="vertical"
@@ -161,28 +152,11 @@ const Filter = (props) => {
                   >
                     Timestamp
                   </a>
-                </div>
-              </div>
+                </div> */}
+          {/* </div>
             </div>
-          )}
+          )} */}
         </div>
-        {/* <select
-            onChange={(e) => {
-              setSelection(e.target.value);
-            }}
-          >
-            <option defaultValue="options">options</option>
-            <option value="container_id">container id</option>
-            <option value="container_name">name</option>
-            <option value="container_image">image</option>
-            <option value="status">status</option>
-            <option value="stream">stream</option>
-            <option value="timestamp">timestamp</option>
-            <option value="host_ip">host ip</option>
-            <option value="host_port">host port</option>
-            <option value="log_level">log level</option>
-          </select> */}
-
         {/* conditionally renders input element for specfic options */}
         {(selection === 'container_id' ||
           selection === 'container_name' ||
@@ -235,40 +209,33 @@ const Filter = (props) => {
 
         {/* conditionally renders stream options */}
         {selection === 'stream' && (
-          <>
-            <div className="inline-block relative w-48">
-              {/* className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150" */}
-              <select
-                onChange={(e) => {
-                  if (!props.filterOptions.stream.includes(e.target.value)) {
-                    props.setFilterOptions({
-                      ...props.filterOptions,
-                      stream: [...props.filterOptions.stream, e.target.value],
-                    });
-                  }
-                }}
-                className="block appearance-none w-full rounded-md bg-white border border-gray-300 text-sm text-gray-700 hover:text-gray-500 mx-4 px-4 py-2 pr-8 leading-tight focus:outline-none"
+          <div className="inline-block relative w-48">
+            {/* className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150" */}
+            <select
+              onChange={(e) => {
+                if (!props.filterOptions.stream.includes(e.target.value)) {
+                  props.setFilterOptions({
+                    ...props.filterOptions,
+                    stream: [...props.filterOptions.stream, e.target.value],
+                  });
+                }
+              }}
+              className="block appearance-none w-full rounded-md bg-white border border-gray-300 text-sm text-gray-700 hover:text-gray-500 mx-4 px-4 py-2 pr-8 leading-tight focus:outline-none"
+            >
+              <option>Select Stream</option>
+              <option>stdout</option>
+              <option>stderr</option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg
+                className="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
               >
-                <option>Select Stream</option>
-                <option>stdout</option>
-                <option>stderr</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
             </div>
-            {/* <select>
-              <option defaultValue="options">select stream</option>
-              <option value="stdout">stdout</option>
-              <option value="stderr">stderr</option>
-            </select> */}
-          </>
+          </div>
         )}
 
         {/* conditionally renders timestamp options */}
---
+++ app/components/FilterOptions.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const FilterOptions = ({ setSelection, setIsOpen }) => {
+  const filterOptions = [
+    'container_id',
+    'container_name',
+    'container_image',
+    'host_port',
+    'stream',
+    'timestamp',
+  ];
+
+  const filterOptionsAsComponents = filterOptions.map((filter) => {
+    // Remove snakecase and capitalize first word of every filter to use as a label
+    const str = filter.replace('_', ' ');
+    const label = str[0].toUpperCase() + str.substring(1);
+    return (
+      <a
+        onClick={(e) => {
+          const val = e.currentTarget.id.match(/(?<=-).*$/g)[0];
+          setSelection(val);
+          setIsOpen(false);
+        }}
+        id={`filter-${filter}`}
+        href="#"
+        className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+        role="menuitem"
+      >
+        {label}
+      </a>
+    );
+  });
+
+  return (
+    <div className="origin-top-right absolute left-0 mt-2 py-1 w-56 rounded-md shadow-lg">
+      <div className="rounded-md bg-white shadow-xs">
+        <div
+          className="py-1"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          {filterOptionsAsComponents}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterOptions;
