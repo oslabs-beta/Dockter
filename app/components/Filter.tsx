3843c8022719795813a7ece26367377611873cfa
Sam Kim <samuyyy@gmail.com>
2020-11-02 23:33:08 -0500

    Clean up

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -69,6 +69,7 @@ const Filter = (props) => {
             <FilterOptions setSelection={setSelection} setIsOpen={setIsOpen} />
           )}
         </div>
+
         {/* Conditionally renders input field only for the following options */}
         {(selection === 'container_id' ||
           selection === 'container_name' ||
@@ -85,46 +86,6 @@ const Filter = (props) => {
             filterOptions={props.filterOptions}
             setFilterOptions={props.setFilterOptions}
           />
-          // <form className="w-full max-w-sm inline-block mx-4">
-          //   <div className="flex items-center border rounded-md border-teal-500">
-          //     <input
-          //       id="filter-input"
-          //       className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-          //       type="text"
-          //       placeholder={`Enter a ${selection.replace('_', ' ')}...`}
-          //       aria-label="Full name"
-          //       onChange={(e) => {
-          //         setUserInput(e.target.value);
-          //       }}
-          //       value={userInput}
-          //     ></input>
-          //     <button
-          //       className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded-r"
-          //       type="button"
-          //       onClick={(e) => {
-          //         e.preventDefault();
-
-          //         // TODO: Add proper error handling
-          //         if (userInput === '') return;
-
-          //         if (!props.filterOptions[selection].includes(userInput)) {
-          //           props.setFilterOptions({
-          //             ...props.filterOptions,
-          //             [selection]: [
-          //               ...props.filterOptions[selection],
-          //               userInput,
-          //             ],
-          //           });
-          //         }
-
-          //         // Clear input field
-          //         setUserInput('');
-          //       }}
-          //     >
-          //       Submit
-          //     </button>
-          //   </div>
-          // </form>
         )}
 
         {/* Conditionally renders stream options */}
