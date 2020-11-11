30d907e4c140c6aae8ca29943edb986b5af4b286
Sam Kim <samuyyy@gmail.com>
2020-11-11 17:08:09 -0500

    Fix margin issue for FilterStreamOptions.tsx

--- app/components/FilterStreamOptions.tsx
+++ app/components/FilterStreamOptions.tsx
@@ -16,21 +16,12 @@ const FilterStreamOptions = ({ filterOptions, setFilterOptions }) => {
             });
           }
         }}
-        className="block appearance-none w-full rounded-md bg-white border border-gray-300 text-sm text-gray-700 hover:text-gray-500 mx-4 px-4 py-2 pr-8 leading-tight focus:outline-none"
+        className="block appearance-none rounded-md bg-white border border-gray-300 text-sm text-gray-700 hover:text-gray-500 mx-4 px-4 py-2 pr-8 leading-tight focus:outline-none"
       >
         <option>Select Stream</option>
         <option>stdout</option>
         <option>stderr</option>
       </select>
-      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg
-          className="fill-current h-4 w-4"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-        </svg>
-      </div>
     </div>
   );
 };
