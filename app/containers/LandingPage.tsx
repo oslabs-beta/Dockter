8c2caaad90a4b1130ed852886fe2e68cfcd72b05
Sam Kim <samuyyy@gmail.com>
2020-11-11 04:41:29 -0500

    Remove ready-hidden emitter

--- app/containers/LandingPage.tsx
+++ app/containers/LandingPage.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import LogsContainer from './LogsContainer';
 import Navbar from '../components/Navbar';
-// TODO: Investigate where to put IPC Event Emitter for channel ready 
+// TODO: Investigate where to put IPC Event Emitter for channel ready
 import { ipcRenderer } from 'electron';
 
 export default function LandingPage() {
   ipcRenderer.send('ready');
-  ipcRenderer.send('ready-hidden');
 
   return (
     <>
