7e9b6693f2ab319bf0dbcd6c0a2728ff05253e9c
Sam Kim <samuyyy@gmail.com>
2020-11-11 21:32:05 -0500

    Navbar: Add small logo and implement LiveLogController

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import LiveLogController from './LiveLogController';
 
-const Navbar = (props) => {
+const Navbar = ({ listeningForNewLogs, setListeningForNewLogs }) => {
   return (
     <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -10,27 +11,47 @@ const Navbar = (props) => {
             <div className="flex-shrink-0">
               <img
                 className="block lg:hidden h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-on-dark.svg"
-                alt="Workflow logo"
+                src="./assets/DockterLogoSM.png"
+                alt="Dockter logo"
               />
               <img
                 className="hidden lg:block h-10 w-auto"
                 src="./assets/DockterLogo.png"
-                alt="Workflow logo"
+                alt="Dockter logo"
               />
             </div>
             <div className="hidden sm:block sm:ml-6">
-              <div className="flex">
-                <a
-                  href="#"
-                  className="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
-                >
-                  Logs
-                </a>
-              </div>
+              <div className="flex"></div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div>
+          <div>
+            <LiveLogController
+              listeningForNewLogs={listeningForNewLogs}
+              setListeningForNewLogs={setListeningForNewLogs}
+            />
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {listeningForNewLogs ? (
+              <>
+                <span className="text-gray-400 text-sm">
+                  Collecting Live Logs
+                </span>
+                <span className="flex h-3 w-3 pl-2 mt-1">
+                  <span className="animate-ping absolute inline-flex pl-2 h-2 w-2 rounded-full bg-green-200 opacity-75"></span>
+                  <span className="relative inline-flex rounded-full pl-2 h-2 w-2 bg-green-400"></span>
+                </span>
+              </>
+            ) : (
+              <>
+                <span className="text-gray-400 text-sm">
+                  Live Logs Collection Paused
+                </span>
+                <span className="flex h-3 w-3 pl-2 mt-1">
+                  <span className="relative inline-flex rounded-full pl-2 h-2 w-2 bg-red-400"></span>
+                </span>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </nav>
