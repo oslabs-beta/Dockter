0af12a8d9cbeea9080855cc679435428e17e47fd
Sam Kim <samuyyy@gmail.com>
2021-01-18 21:16:02 -0500

    modify stream listeners for InfluxDB setup

--- server/server.js
+++ server/server.js
@@ -1,149 +1,263 @@
 const express = require('express');
 const path = require('path');
+const stream = require('stream');
 const Docker = require('dockerode');
+
+const influx = require('./db');
 
 const app = express();
 const PORT = 3000;
 
 const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
-const startup = async () => {
+const initalizeDB = async () => {
   try {
-    const containers = await docker.listContainers({ all: true });
-    const liveLogStreams = await containers.map(async (container) => {
-      const { Id, Image, Status, Names, Ports } = container;
+    await influx.createDatabase('dockter');
 
-      // Remove logs where timestamp >= timeSinceLastLog to avoid duplication
-      // await Log.deleteMany({
-      //   container_id: Id,
-      //   timestamp: { $gte: applicationStartTime },
-      // });
+    const startLogCollection = async () => {
+      try {
+        const containers = await docker.listContainers({ all: true });
 
-      const c = docker.getContainer(container.Id);
+        containers.forEach(async (container) => {
+          try {
+            const { Id, Image, Status, Names, Ports } = container;
 
-      // Initiate following logs for container
-      const log = await c.logs({
-        follow: true,
-        stdout: true,
-        stderr: true,
-        timestamps: true,
-        since: applicationStartTimeUnix,
-      });
+            if (!Names.some((name) => name.match(/^\/dockter/g))) {
+              console.log(Names);
+              const c = docker.getContainer(Id);
 
-      if (log) {
-        const stdout = new stream.PassThrough();
-        const stderr = new stream.PassThrough();
+              const log = await c.logs({
+                follow: true,
+                stdout: true,
+                stderr: true,
+                timestamps: true,
+              });
 
-        c.modem.demuxStream(log, stdout, stderr);
-        stdout.on('data', async (chunk) => {
-          const chunkString = chunk.toString();
-          const newLog = {
-            message: chunkString.slice(30),
-            container_id: Id,
-            container_name: Names[0],
-            container_image: Image,
-            timestamp: new Date(chunkString.slice(0, 30)),
-            stream: 'stdout',
-            status: Status,
-            ports: Ports,
-          };
+              if (log) {
+                const stdout = new stream.PassThrough();
+                const stderr = new stream.PassThrough();
 
-          // TODO: Change this to InfluxDB query
-          const newLogToSend = await Log.findOneAndUpdate(
-            newLog,
-            { $set: newLog },
-            { upsert: true, new: true }
-          );
+                c.modem.demuxStream(log, stdout, stderr);
 
-          // Container should only update if hidden-log collector has finished collecting past logs
-          // if (pastLogCollectionComplete[Id]) {
-          //   await Container.findOneAndUpdate(
-          //     { container_id: Id },
-          //     { $set: { last_log: new Date(chunkString.slice(0, 30)) } },
-          //     { upsert: true, new: true }
-          //   );
-          // }
+                stdout.on('data', async (chunk) => {
+                  try {
+                    const chunkString = chunk.toString();
 
-          // TODO: Chagne this to websockets
-          content.send('newLog', {
-            ...newLogToSend,
-            _id: newLogToSend._id.toString(),
-            _doc: {
-              ...newLogToSend._doc,
-              _id: newLogToSend._doc._id.toString(),
-            },
-          });
+                    // TODO: Look into continuous queries
+                    influx.writePoints([
+                      {
+                        measurement: 'logs',
+                        tags: {
+                          stream: 'stdout',
+                          container_name: Names[0],
+                        },
+                        fields: {
+                          message: chunkString.slice(30),
+                        },
+                        timestamp: new Date(chunkString.slice(0, 30)),
+                      },
+                    ]);
+                  } catch (e) {
+                    console.log(e);
+                  }
+                });
+
+                stderr.on('data', async (chunk) => {
+                  try {
+                    const chunkString = chunk.toString();
+
+                    // TODO: Look into continuous queries
+                    await influx.writePoints([
+                      {
+                        measurement: 'logs',
+                        tags: {
+                          stream: 'stderr',
+                          container_name: Names[0],
+                        },
+                        fields: {
+                          message: chunkString.slice(30),
+                        },
+                        timestamp: new Date(chunkString.slice(0, 30)),
+                      },
+                    ]);
+
+                    const containerNames = [
+                      '/test-influx',
+                      '/docker_getting-started',
+                    ];
+
+                    const reg = new RegExp(containerNames.join('|'));
+
+                    const queryResult = await influx.query(
+                      `
+                      select * from logs
+                      where container_name =~ ${reg}
+                      `
+                    );
+
+                    queryResult.forEach((row) => {
+                      console.log(
+                        'query result should go here >>>>>>>>>>',
+                        row
+                      );
+                    });
+                  } catch (e) {
+                    console.log(e);
+                  }
+                });
+              }
+            }
+          } catch (e) {
+            console.log(e);
+          }
         });
-
-        stderr.on('data', async (chunk) => {
-          const chunkString = chunk.toString();
-          const newLog = {
-            message: chunkString.slice(30),
-            container_id: Id,
-            container_name: Names[0],
-            container_image: Image,
-            timestamp: new Date(chunkString.slice(0, 30)),
-            stream: 'stderr',
-            status: Status,
-            ports: Ports,
-          };
-
-          // TODO: Change this to InfluxDB query
-          const newLogToSend = await Log.findOneAndUpdate(
-            newLog,
-            { $set: newLog },
-            { upsert: true, new: true }
-          );
-
-          // Container should only update if hidden-log collector has finished collecting past logs
-          // if (pastLogCollectionComplete[Id]) {
-          //   await Container.findOneAndUpdate(
-          //     { container_id: Id },
-          //     { $set: { last_log: new Date(chunkString.slice(0, 30)) } },
-          //     { upsert: true, new: true }
-          //   );
-          // }
-
-          // TODO: Change this to websockets
-          content.send('newLog', {
-            ...newLogToSend,
-            _id: newLogToSend._id.toString(),
-            _doc: {
-              ...newLogToSend._doc,
-              _id: newLogToSend._doc._id.toString(),
-            },
-          });
-        });
-
-        return [stdout, stderr];
+      } catch (e) {
+        console.log(e);
       }
-    });
+    };
 
-    const streams = await Promise.all(liveLogStreams);
-
-    // TODO: Change this to websockets
-    ipcMain.on('pauseLiveLogs', () => {
-      console.log('pause all streams');
-      streams.forEach(([stdout, stderr]) => {
-        stdout.pause();
-        stderr.pause();
-      });
-    });
-
-    // TODO: Change this to websockets
-    ipcMain.on('resumeLiveLogs', () => {
-      console.log('resume all streams');
-      streams.forEach(([stdout, stderr]) => {
-        stdout.resume();
-        stderr.resume();
-      });
-    });
+    startLogCollection();
   } catch (e) {
-    console.log(`Error: ${e}`);
+    console.log('db initialization error:', e);
   }
 };
 
-// startup();
+initalizeDB();
+// const startup = async () => {
+//   try {
+//     const containers = await docker.listContainers({ all: true });
+//     const liveLogStreams = await containers.map(async (container) => {
+//       const { Id, Image, Status, Names, Ports } = container;
+
+//       // Remove logs where timestamp >= timeSinceLastLog to avoid duplication
+//       // await Log.deleteMany({
+//       //   container_id: Id,
+//       //   timestamp: { $gte: applicationStartTime },
+//       // });
+
+//       const c = docker.getContainer(container.Id);
+
+//       // Initiate following logs for container
+//       const log = await c.logs({
+//         follow: true,
+//         stdout: true,
+//         stderr: true,
+//         timestamps: true,
+//         since: applicationStartTimeUnix,
+//       });
+
+//       if (log) {
+//         const stdout = new stream.PassThrough();
+//         const stderr = new stream.PassThrough();
+
+//         c.modem.demuxStream(log, stdout, stderr);
+//         stdout.on('data', async (chunk) => {
+//           const chunkString = chunk.toString();
+//           const newLog = {
+//             message: chunkString.slice(30),
+//             container_id: Id,
+//             container_name: Names[0],
+//             container_image: Image,
+//             timestamp: new Date(chunkString.slice(0, 30)),
+//             stream: 'stdout',
+//             status: Status,
+//             ports: Ports,
+//           };
+
+//           // TODO: Change this to InfluxDB query
+//           const newLogToSend = await Log.findOneAndUpdate(
+//             newLog,
+//             { $set: newLog },
+//             { upsert: true, new: true }
+//           );
+
+//           // Container should only update if hidden-log collector has finished collecting past logs
+//           // if (pastLogCollectionComplete[Id]) {
+//           //   await Container.findOneAndUpdate(
+//           //     { container_id: Id },
+//           //     { $set: { last_log: new Date(chunkString.slice(0, 30)) } },
+//           //     { upsert: true, new: true }
+//           //   );
+//           // }
+
+//           // TODO: Chagne this to websockets
+//           content.send('newLog', {
+//             ...newLogToSend,
+//             _id: newLogToSend._id.toString(),
+//             _doc: {
+//               ...newLogToSend._doc,
+//               _id: newLogToSend._doc._id.toString(),
+//             },
+//           });
+//         });
+
+//         stderr.on('data', async (chunk) => {
+//           const chunkString = chunk.toString();
+//           const newLog = {
+//             message: chunkString.slice(30),
+//             container_id: Id,
+//             container_name: Names[0],
+//             container_image: Image,
+//             timestamp: new Date(chunkString.slice(0, 30)),
+//             stream: 'stderr',
+//             status: Status,
+//             ports: Ports,
+//           };
+
+//           // TODO: Change this to InfluxDB query
+//           const newLogToSend = await Log.findOneAndUpdate(
+//             newLog,
+//             { $set: newLog },
+//             { upsert: true, new: true }
+//           );
+
+//           // Container should only update if hidden-log collector has finished collecting past logs
+//           // if (pastLogCollectionComplete[Id]) {
+//           //   await Container.findOneAndUpdate(
+//           //     { container_id: Id },
+//           //     { $set: { last_log: new Date(chunkString.slice(0, 30)) } },
+//           //     { upsert: true, new: true }
+//           //   );
+//           // }
+
+//           // TODO: Change this to websockets
+//           content.send('newLog', {
+//             ...newLogToSend,
+//             _id: newLogToSend._id.toString(),
+//             _doc: {
+//               ...newLogToSend._doc,
+//               _id: newLogToSend._doc._id.toString(),
+//             },
+//           });
+//         });
+
+//         return [stdout, stderr];
+//       }
+//     });
+
+//     const streams = await Promise.all(liveLogStreams);
+
+//     // TODO: Change this to websockets
+//     ipcMain.on('pauseLiveLogs', () => {
+//       console.log('pause all streams');
+//       streams.forEach(([stdout, stderr]) => {
+//         stdout.pause();
+//         stderr.pause();
+//       });
+//     });
+
+//     // TODO: Change this to websockets
+//     ipcMain.on('resumeLiveLogs', () => {
+//       console.log('resume all streams');
+//       streams.forEach(([stdout, stderr]) => {
+//         stdout.resume();
+//         stderr.resume();
+//       });
+//     });
+//   } catch (e) {
+//     console.log(`Error: ${e}`);
+//   }
+// };
 
 app.use(express.static('public'));
 
